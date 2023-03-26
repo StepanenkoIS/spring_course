17c8d32c6feac629571bc73bb7a417cbf12da7cf
istepanenko <istepanenko@neoflex.ru>
2023-03-26 13:43:33 +0400

    Разел 2. 25. Advice тип @Before

---
+++ spring_course/src/main/java/aop/aspects/LoggingAspect.java
@@ -0,0 +1,21 @@
+package aop.aspects;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+
+  @Before("execution(public void getBook())")
+  public void beforeGetBookAdvice() {
+    System.out.println(LoggingAspect.class.getDeclaredMethods()[0].getName() + ": попытка получить книгу");
+  }
+
+
+}
