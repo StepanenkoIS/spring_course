f8329d38f0addeef4921215da96397974f43bfb3
istepanenko <istepanenko@neoflex.ru>
2023-04-01 16:46:20 +0400

    Выражение Pointcut. Часть 1

--- spring_course/src/main/java/aop/aspects/LoggingAspect.java
+++ spring_course/src/main/java/aop/aspects/LoggingAspect.java
@@ -12,10 +12,25 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
 
-  @Before("execution(public void getBook())")
+  @Before("execution(public void aop.AbstractLibrary.getBook())")
   public void beforeGetBookAdvice() {
-    System.out.println(LoggingAspect.class.getDeclaredMethods()[0].getName() + ": попытка получить книгу");
+    System.out.println("beforeGetBookAdvice(): попытка получить книгу");
   }
 
+  @Before("execution(public void get*())")
+  public void beforeGetAdvice() {
+    System.out.println("beforeGetAdvice(): попытка вызова метода get*()");
+  }
+
+
+  @Before("execution(public * returnBook())")
+  public void beforeReturnBookAdvice() {
+    System.out.println("beforeReturnBookAdvice(): попытка вернуть книгу");
+  }
+
+  @Before("execution(* *())")
+  public void beforeForAllAdvice() {
+    System.out.println("beforeForAllAdvice(): для всех методов");
+  }
 
 }
