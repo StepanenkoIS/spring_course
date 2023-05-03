1cd81c46ecf18234eb309ef28909a051fc535fc5
istepanenko <istepanenko@neoflex.ru>
2023-05-03 12:52:07 +0400

    60. Аннотация @RequestParam

--- spring_course_mvc/src/main/java/com/is/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/com/is/spring/mvc/MyController.java
@@ -4,6 +4,7 @@ package com.is.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,15 +27,22 @@ public class MyController {
 //  public String showEmployeeDetails() {
 //    return "show-emp-details-view";
 //  }
+//
+//  @RequestMapping("/showDetails")
+//  public String showEmployeeDetails(HttpServletRequest request, Model model) {
+//    String empName = request.getParameter("employeeName");
+//    empName = "Mr. " + empName;
+//    model.addAttribute("nameAttribute", empName);
+//    model.addAttribute("description", " - student");
+//
+//    return "show-emp-details-view";
+//  }
+
 
   @RequestMapping("/showDetails")
-  public String showEmployeeDetails(HttpServletRequest request, Model model) {
-    String empName = request.getParameter("employeeName");
-    empName = "Mr. " + empName;
+  public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
+    empName = "Mr. " + empName + " !!!!";
     model.addAttribute("nameAttribute", empName);
-    model.addAttribute("description", " - student");
-
     return "show-emp-details-view";
   }
-
 }
