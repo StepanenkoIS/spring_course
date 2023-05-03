b84f75b6894edfc30a2e6906bc24061d987aaefc
istepanenko <istepanenko@neoflex.ru>
2023-05-03 11:16:20 +0400

    57. Первое Spring MVC приложение

---
+++ spring_course_mvc/src/main/java/com/is/spring/mvc/MyController.java
@@ -0,0 +1,16 @@
+package com.is.spring.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+
+  @RequestMapping("/")
+  public String showFirstView() {
+    return "first-view";
+  }
+
+}
---
+++ spring_course_mvc/src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2> This is Spring MVC !!!</h2>
+
+</body>
+
+</html>>
