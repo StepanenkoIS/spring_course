46ded6a3df06f62f44704fd0b3910d39b086c463
istepanenko <istepanenko@neoflex.ru>
2023-05-04 20:07:12 +0400

    70. Отображение списка всех работников

---
+++ spring_course_mvc_hibernate_aop/src/main/java/com/is/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,31 @@
+package com.is.spring.mvc_hibernate_aop.dao;
+
+import com.is.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+  @Autowired
+  private SessionFactory sessionFactory;
+
+  @Transactional
+  public List<Employee> getAllEmployees() {
+
+    Session session = sessionFactory.getCurrentSession();
+//    List<Employee> employeeList = session.createQuery("from Employee", Employee.class).getResultList();
+    Query<Employee> query = session.createQuery("from Employee", Employee.class);
+    List<Employee> employeeList = query.getResultList();
+
+    return employeeList;
+  }
+}
