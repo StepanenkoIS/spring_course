607224ca61de1349a973d46f75c810da3b7cf55b
istepanenko <istepanenko@neoflex.ru>
2023-05-08 14:44:13 +0400

    82. Получение одного работника по ID

--- spring_course_rest/src/main/java/com/is/spring/rest/controller/MyRESTController.java
+++ spring_course_rest/src/main/java/com/is/spring/rest/controller/MyRESTController.java
@@ -5,6 +5,7 @@ import com.is.spring.rest.entity.Employee;
 import com.is.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,10 @@ public class MyRESTController {
     return employees;
   }
 
+  @GetMapping("/employees/{id}")
+public Employee getEmployee(@PathVariable int id) {
+    Employee employee = employeeService.getEmployee(id);
+    return employee;
+  }
 
 }
