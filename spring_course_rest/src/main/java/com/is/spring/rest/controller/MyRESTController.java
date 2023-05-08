1ee215693b2efb678a351ff54ff3511658996c3a
istepanenko <istepanenko@neoflex.ru>
2023-05-08 20:41:07 +0400

    85. Добавление нового работника

--- spring_course_rest/src/main/java/com/is/spring/rest/controller/MyRESTController.java
+++ spring_course_rest/src/main/java/com/is/spring/rest/controller/MyRESTController.java
@@ -2,12 +2,9 @@ package com.is.spring.rest.controller;
 
 
 import com.is.spring.rest.entity.Employee;
-import com.is.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.is.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.is.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,12 +26,19 @@ public class MyRESTController {
   }
 
   @GetMapping("/employees/{id}")
-public Employee getEmployee(@PathVariable int id) {
+  public Employee getEmployee(@PathVariable int id) {
     Employee employee = employeeService.getEmployee(id);
     if (employee == null) {
-        throw new NoSuchEmployeeException("There is no employee with ID = " + id + " int Database");
+      throw new NoSuchEmployeeException("There is no employee with ID = " + id + " int Database");
     }
     return employee;
+  }
+
+  @PostMapping("/employees")
+  public Employee addNewEmployee(@RequestBody Employee employee) {
+    employeeService.saveEmployee(employee);
+    return employee;
+
   }
 
 }
