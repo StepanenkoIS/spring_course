6d1ae248f6398101aceea7057f02fb9c69805ff5
istepanenko <istepanenko@neoflex.ru>
2023-05-09 17:08:56 +0400

    92. Процедура авторизации

--- spring_course_security/src/main/java/com/is/spring/security/configuration/MySecurityConfig.java
+++ spring_course_security/src/main/java/com/is/spring/security/configuration/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.is.spring.security.configuration;
 
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -17,5 +18,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
       .withUser(userBuilder.username("semen").password("semen").roles("EMPLOYEE"))
       .withUser(userBuilder.username("elena").password("elena").roles("HR"))
       .withUser(userBuilder.username("vlad").password("vlad").roles("MANAGER", "HR"));
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+      .antMatchers("/hr_info").hasRole("HR")
+      .antMatchers("/manager_info/**").hasRole("MANAGER")
+      .and().formLogin().permitAll();
   }
 }
