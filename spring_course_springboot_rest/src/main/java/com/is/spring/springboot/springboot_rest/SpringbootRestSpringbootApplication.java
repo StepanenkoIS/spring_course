df834356099a6e16933b206cefc339a2b632e056
istepanenko <istepanenko@neoflex.ru>
2023-05-09 19:25:16 +0400

    96. Создание Spring Boot приложения

---
+++ spring_course_springboot_rest/src/main/java/com/is/spring/springboot/springboot_rest/SpringbootRestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.is.spring.springboot.springboot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.is.spring.springboot.springboot_rest")
+public class SpringbootRestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestSpringbootApplication.class, args);
+	}
+
+}
